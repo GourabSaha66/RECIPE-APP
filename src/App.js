7c50abe947e487f8e9d91b02fade66a3a3a78e33
Gourab <sgourab66@gmail.com>
2023-10-06 18:40:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Recipeid from "./components/Recipeid";
+import Category from "./components/Category";
+import SearchElement from "./components/SearchElement";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:idMeal" element={<Recipeid />} />
+          <Route path="/category/:name" element={<Category />} />
+          <Route path="/search/:searchTerm" element={<SearchElement />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/TrendingSlider.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
+
+const TrendingSlider = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const ferchData = async () => {
+      const api = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian`
+      );
+      const data = await api.json();
+      // console.log(data)
+      setData(data.meals);
+    };
+    ferchData();
+  }, []);
+
+  var settings = {
+    // dots: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 500,
+    cssEase: "linear",
+  };
+  return (
+    <>
+      <div
+        style={{
+          height: "26vh",
+          width: "99%",
+          margin: "auto",
+          overflowX: 'hidden'
+        }}
+      >
+        <Slider
+          {...settings}
+          style={{
+            marginTop: "1rem",
+          }}
+        >
+          {data.map((d) => {
+            return (
+              <Link to={`/${d.idMeal}`} key={d.idMeal}>
+                <div className="slider2">
+                  <img
+                    src={d.strMealThumb}
+                    alt=""
+                    style={{ width: "10rem", height: "7rem" }}
+                  />
+                </div>
+              </Link>
+            );
+          })}
+        </Slider>
+      </div>
+    </>
+  );
+};
+
+export default TrendingSlider;
